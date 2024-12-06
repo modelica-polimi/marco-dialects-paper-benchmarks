6bdc424687fd632fa7b1e0ac4dc3e16024af9e40
Michele Scuttari <michele.scuttari@outlook.com>
2024-12-06 21:30:38 +0100

    Removed unused configuration

--- data/scripts/omc/compile_simulate_all.sh
+++ data/scripts/omc/compile_simulate_all.sh
@@ -22,4 +22,3 @@ sim_args=${@:2}
 #"$path/compile_simulate_config.sh" 183 183 61 $solver $sim_args || exit 1
 #"$path/compile_simulate_config.sh" 246 246 82 $solver $sim_args || exit 1
 #"$path/compile_simulate_config.sh" 312 312 104 $solver $sim_args || exit 1
-#"$path/compile_simulate_config.sh" 393 393 131 $solver $sim_args || exit 1
