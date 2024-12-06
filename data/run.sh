f95460362eacd35f3fa57582882c1db5f938d313
Michele Scuttari <michele.scuttari@outlook.com>
2024-12-06 23:07:12 +0100

    Call csv_exporter install script

--- data/run.sh
+++ data/run.sh
@@ -4,5 +4,6 @@ path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cat /etc/hostname
 cat /proc/cpuinfo
+"$path/scripts/install_csv_exporter.sh" 
 "$path/scripts/install_marco.sh" 
 "$path/scripts/run.sh"
