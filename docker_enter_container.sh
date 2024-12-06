5eb6d55d9be9fd5ec023d6bda32b6cb5f5a9f4b0
Michele Scuttari <michele.scuttari@outlook.com>
2024-12-06 22:29:58 +0100

    Remove unused script

--- docker_enter_container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-docker run --rm -ti -v "$path/data":/data -v "$path/marco":/tmp/marco-src -v "$path/output":/output marco-benchmarks
