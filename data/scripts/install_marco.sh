56a93ae49809562c0d08bc5d157d900926557766
Michele Scuttari <michele.scuttari@outlook.com>
2024-12-06 22:30:41 +0100

    Clean MARCO install folder before build

--- data/scripts/install_marco.sh
+++ data/scripts/install_marco.sh
@@ -3,6 +3,9 @@
 rm -rf /tmp/marco-build
 mkdir /tmp/marco-build
 
+rm -rf /root/install/marco
+mkdir /root/install/marco
+
 cmake \
   -S /tmp/marco-src -B /tmp/marco-build -G Ninja \
   -DCMAKE_LINKER_TYPE=MOLD \
