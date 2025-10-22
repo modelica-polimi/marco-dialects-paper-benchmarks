b500cac32628228cefaaa928357af62b60db8083
Michele Scuttari <michele.scuttari@outlook.com>
2025-10-22 11:34:06 +0200

    Fix CMake invocation

--- data/models/MethanolHeatExchangers/cpp/compile.sh
+++ data/models/MethanolHeatExchangers/cpp/compile.sh
@@ -12,7 +12,7 @@ du -scb $SRC_DIR/*.h $SRC_DIR/*.c $SRC_DIR/*.cpp $SRC_DIR/*.hpp | tail -n1 > $C_
 cmake -S $SRC_DIR -B $BUILD_DIR
 
 COMPILATION_TIME_FILE=$STATS_DIR/cpp-compilation-time-$EXPERIMENT.txt
-/usr/bin/time -p -a -o $COMPILATION_TIME_FILE cmake -B $BUILD_DIR $COMPILATION_ARGS -- -j || exit 1
+/usr/bin/time -p -a -o $COMPILATION_TIME_FILE cmake --build -B $BUILD_DIR $COMPILATION_ARGS -- -j || exit 1
 
 # Get the binary size
 BINARY_SIZE_FILE=$STATS_DIR/cpp-binary-size-$EXPERIMENT.txt
